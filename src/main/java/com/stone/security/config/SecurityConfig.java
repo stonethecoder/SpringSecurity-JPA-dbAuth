3dbbc6bb9286b128f7c0506b34b75a53fc0b1841
Stone <Stone@dell>
2018-11-10 15:56:11 +0530

    2nd commit

--- src/main/java/com/stone/security/config/SecurityConfig.java
+++ src/main/java/com/stone/security/config/SecurityConfig.java
@@ -38,8 +38,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 			.and().authorizeRequests()
 			.antMatchers("/admin/**").hasRole("ADMIN")
 			.anyRequest().authenticated();
-			/*.and()
-			.formLogin().permitAll();*/
+			.and()
+			.formLogin().permitAll();
 		
 			
 	}
